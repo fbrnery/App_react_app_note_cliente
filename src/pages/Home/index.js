9b6028bfb89318d0d8efaa97d6d51e7cac1101c8
Fabricio Miranda <fbrnery@gmail.com>
2022-07-07 13:14:38 -0300

    Terminei o Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,33 +1,34 @@
-import React, { Fragment } from 'react';
-import { Link } from 'react-router-dom';
-import presentationImage from '../../assets/images/presentation.png';
-import './index.css'
-
+import React, { Fragment } from "react";
+import { Link } from "react-router-dom";
+import presentationImage from "../../assets/images/presentation.png";
+import "./index.css";
 
 function Home() {
   return (
     <Fragment>
-    <div class="container-fluid">
-  <div class="row">
-    <div class="col">
-    <h1 class="display-5 fw-bold"> Create notes easily and access when you wants on the cloud</h1>
-        <p class="col-md-8 fs-4">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.<br/><br/>
-Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print.</p>
-        <button class="btn btn-primary btn-lg" type="button">Register for free Nowstrong</button>
-    </div>
-    <div class="col">
-    <a className="navbar-brand" href="#">
-            <img src={presentationImage} />
-          </a>
-    </div>
-  </div>
-   </div>
-
-
-</Fragment>
- 
-
-      );
+      <div class="container-fluid">
+      <div class="col">
+        <img
+          src={presentationImage}
+          class="col-md-6 float-md-end mb-3 ms-md-3"
+          alt="..."
+        />
+        <h2> Create notes easily and access when you wants on the cloud</h2>
+        <h6>
+          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in
+          laying out print, graphic or web designs.
+          <br />
+          <br />
+          Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in
+          laying out print.
+        </h6>
+        <button type="button" class="btn btn-outline-info">
+          Register for free Nowstrong
+        </button>
+        </div>
+        </div>
+    </Fragment>
+  );
 }
 
-      export default Home;
+export default Home;
