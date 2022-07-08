219155dac189a18df88ab0cfefd30e26747749cc
Fabricio Miranda <fbrnery@gmail.com>
2022-07-08 14:39:46 -0300

    Tela de registro

--- src/screens/auth/register/index.js
+++ src/screens/auth/register/index.js
@@ -1,14 +1,24 @@
 import React, { Fragment } from 'react';
 import { Link } from "react-router-dom";
+import Header from '../../../component/Header';
+import LogoImage from '../../../assets/images/logo.png';
+import './index.css'
 
 function Register() {
     return (
-      <div>
         <Fragment>
-            <h1> Bem vindo, a página Register !!!</h1>
-        </Fragment>
-      </div>
+            <div class="card-group">
+                <div class="card">
+                    <img src={LogoImage} class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <h5 class="card-title">Card Notes</h5>
+                        <p class="card-text"> Your notes on the cloud</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+                </div>
+            </div>
+            </Fragment >
     );
-  }
+}
 
 export default Register;
