416ce114e7f11cbbe396c97fb66cd0e1d63f872c
Fabricio Miranda <fbrnery@gmail.com>
2022-08-12 16:13:08 -0300

    Component do formulário de registro

--- src/component/auth/register_form/index.js
+++ src/component/auth/register_form/index.js
@@ -3,55 +3,88 @@ import { Redirect } from "react-router-dom";
 import "./index.css";
 
 function RegisterForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [redirectToLogin, setRedirectToLogin] = useState(false);
+  const [error, setError] = useState(false);
+
+  if (redirectToLogin) return <Redirect to={{ pathname: "/login" }} />;
+
   return (
-    <Fragment>
-      <div class="container-fluid">
-        <div className="mb-3">
-          <label for="exampleFormControlInput1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleFormControlInput1"
-            placeholder="name@example.com"
-          />
-        </div>
-        <div className="mb-3">
-          <label for="exampleInputEmail1" class="form-label">
-            Email address
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="exampleInputEmail1"
-            aria-describedby="emailHelp"
-          />
-          <div id="emailHelp" class="form-text">
-            We'll never share your email with anyone else.
+    <fragment>
+      <section className="py-5 text-center container">
+        <div className="row py-lg-5">
+          <div className="col-lg-6 col-md-8 mx-auto">
+            <h1 className="fw-light">Your notes on the cloud</h1>
+            <form>
+            <div className="col">
+              
+             <input 
+             value={name}
+             onChange={e => setName(e.target.value)}
+             type="text" 
+             class="form-control" 
+             placeholder="Name" 
+             aria-label="First name"/>
+              </div>
+
+              <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">
+                  
+                </label>
+                <input
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  type="email"
+                  class="form-control"
+                  placeholder="E-mail address" 
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                />
+                <div id="emailHelp" class="form-text">
+                  We'll never share your email with anyone else.
+                </div>
+              </div>
+              <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">
+                  
+                </label>
+                <input
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  type="password"
+                  class="form-control"
+                  placeholder="Password" 
+                  id="exampleInputPassword1"
+                />
+              </div>
+              <div class="mb-3 form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1">
+                  Check me out
+                </label>
+              </div>
+              <div className="button1">
+              <button type="submit" class="btn btn-light" onClick={e => setRedirectToLogin(true)} >
+                Login Or
+              </button>
+              </div>
+              <div className="button2">
+              <button type="submit" class="btn btn-primary">
+                Register
+              </button>
+              </div>
+              { error && <button type="button" class="btn btn-danger">Email or Password invalid</button>}
+            </form>
           </div>
         </div>
-        <div className="mb-3">
-          <label for="exampleInputPassword1" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="exampleInputPassword1"
-          />
-        </div>
-        <div className="buttom">
-          <button type="submit" class="btn btn-primary">
-            Login Or
-          </button>
-          <button type="submit" class="btn btn-outline-primary">
-            Register
-          </button>
-        </div>
-      </div>
-            <form></form>   
-    </Fragment>
+      </section>
+    </fragment>
   );
 }
 
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,26 +6,27 @@ import "./index.css";
 function Home() {
   return (
     <main>
-   <h1 class="visually-hidden">Heroes examples</h1>
+      <h1 class="visually-hidden">Heroes examples</h1>
 
-<div class="px-4 py-5 my-5 text-center">
-<img src={presentationImage}/>
-  <h1 class="display-5 fw-bold">App Note</h1>
-  <div class="col-lg-6 mx-auto">
-    <p class="lead mb-4">
-    The application allows you to organize meetings in a virtual file. 
-    There is even a Premium version, which the user can upload up to 10 GB of data per month.
-    </p>
-    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-    <button type="button" className="btn btn-light">
-    <Link to="/register">Login In</Link>
-    </button>
-    <button type="button" className="btn btn-light">
-    <Link to="/">Sign Up</Link>
-    </button>
-    </div>
-  </div>
-</div>
+      <div class="px-4 py-5 my-5 text-center">
+        <img src={presentationImage} />
+        <h1 class="display-5 fw-bold">App Note</h1>
+        <div class="col-lg-6 mx-auto">
+          <p class="lead mb-4">
+            The application allows you to organize meetings in a virtual file.
+            There is even a Premium version, which the user can upload up to 10
+            GB of data per month.
+          </p>
+          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+            <button type="button" className="btn btn-light">
+              <Link to="/register">Login In</Link>
+            </button>
+            <button type="button" className="btn btn-light">
+              <Link to="/">Sign Up</Link>
+            </button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
