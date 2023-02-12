1ae1a2bfce9285ee4dbdb19ca53630bac013c542
Fabricio Miranda <fbrnery@gmail.com>
2023-02-12 17:32:21 -0300

    Criando rotas protegidas

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import RegisterScreen from "./screens/auth/register";
 import LoginScreen from "./screens/auth/login";
 import NotesIndexScreen from "./screens/notes/index";
 import UserEditScreen from "./screens/users/edit";
+import PrivateRouter from './components/auth/private_route';
 
 const Routes = () => {
   return (
@@ -15,8 +16,8 @@ const Routes = () => {
         <Route exact path="/" component={HomeScreen} />
         <Route exact path="/register" component={RegisterScreen} />
         <Route exact path="/login" component={LoginScreen} />
-        <Route exact path="/notes" component={NotesIndexScreen} />
-        <Route exact path="/users/edit" component={UserEditScreen} />
+        <PrivateRouter exact path="/notes" component={NotesIndexScreen} />
+        <PrivateRouter exact path="/users/edit" component={UserEditScreen} />
       </Switch>
     </BrowserRouter>
   );
