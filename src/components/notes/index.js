506c554c528d6d8b5e5037e5a71ac72431fb903c
Fabricio Miranda <fbrnery@gmail.com>
2023-02-14 10:08:15 -0300

    Estrutura da página de notes-parte 1

---
+++ src/components/notes/index.js
@@ -0,0 +1,12 @@
+import React, { Fragment, useEffect, useState } from "react";
+
+
+const Notes = () => {
+    return(
+        <Fragment>
+            <h1>Notes</h1>
+        </Fragment>
+    )
+}
+
+export default Notes;
--- src/screens/notes/index/index.js
+++ src/screens/notes/index/index.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
 import HeaderLogged from '../../../components/header_logged';
+import Notes from '../../../components/notes'
 
 function NotesScreen() {
     return (
       <Fragment>
           <HeaderLogged />
+          <Notes/>
       </Fragment>
     );
   }
