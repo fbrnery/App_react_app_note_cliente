ace46f7798d432f5f932e1e7da08d494d760aec3
Fabricio Miranda <fbrnery@gmail.com>
2023-02-22 22:24:16 -0300

    Deploy heroku

--- src/components/notes/index.js
+++ src/components/notes/index.js
@@ -1,12 +1,42 @@
 import React, { Fragment, useEffect, useState } from "react";
 
+import List from "../notes/list";
+import NoteService from '../../services/note'
 
-const Notes = () => {
-    return(
-        <Fragment>
-            <h1>Notes</h1>
-        </Fragment>
-    )
+
+function Notes(props) {
+  const [notes, setNotes] = useState([]);
+  const [current_note, setCurrentNote] = useState({ title: "", body: "", id: "" });
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const response = await NoteService.index();
+    if (response.data.length >= 1) {
+      setNotes(response.data.reverse())
+      setCurrentNote(response.data[0])
+    }
+  }
+
+  const selectNote = (id) => {
+    const note = notes.find((note) => {
+      return note._id == id;
+    })
+    setCurrentNote(note);
+  }
+
+  return (
+    <Fragment>
+      <div className="notes" id="notes">
+          <List
+            notes={notes}
+            selectNote={selectNote}
+            current_note={current_note} />
+ </div>
+    </Fragment>
+  )
 }
 
 export default Notes;
---
+++ src/components/notes/list/index.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+import Moment from "moment";
+
+
+function ListNotes(props) {
+  return (
+    <Fragment>
+      
+       
+    </Fragment>
+  );
+}
+
+export default ListNotes;
